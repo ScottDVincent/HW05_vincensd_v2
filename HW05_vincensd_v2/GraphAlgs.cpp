29f78128f45dfb3d6d937b2ae59002e6bcf03385
Scott D Vincent <vincensd@muohio.edu>
2012-11-26 14:16:29 -0500

    inital commit
    2nd version: 1st got a disconnected HEAD error

---
+++ HW05_vincensd_v2/GraphAlgs.cpp
@@ -0,0 +1,29 @@
+/*****
+ * File		: GraphAlgs.cpp	
+ * Author   : Scott Vincent
+ * Date     : 2012-28-10
+ * Purpose  : This program will perform several duties required for HW05.
+ * Sources  : 
+
+
+  * @note This file is (c) 2012. It is licensed under the 
+ * CC BY 3.0 license (http://creativecommons.org/licenses/by/3.0/),
+ * which means you are free to use, share, and remix it as long as you
+ * give attribution. Commercial uses are allowed.
+ *
+ */
+
+//my includes
+#include "GraphAlgs.h"
+
+//c++ includes
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+
+//using
+using namespace std;
